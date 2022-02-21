745988a9b39adb2ec9554c3729f6dfd0c8552789
Mihail <mohonovproduction@yandex.ru>
2022-02-21 20:57:52 +0300

    shortest path function

--- test1.py
+++ test1.py
@@ -5,6 +5,8 @@ import sys
 # l - loop, how infinity
 l = sys.maxsize
 
+nodesNames = ['A', 'B', 'C', 'D', 'E', 'F']
+
 A = [
     [0, 4, l, l, l, l],
     [4, 0, 6, 3, 6, l],
@@ -15,24 +17,50 @@ A = [
 ]
 
 width = len(A)
-print('A edge width: ', width)
+print('A size: ', width, 'x', width)
+
+path = A.copy()
+
+repeats = 0
+calculate_repeats = width * width * width
 
 for k in range(0, width):
     for i in range(0, width):
         for j in range(0, width):
-            # print('k: ', k, '\nn: ', i, '\nj: ', j, '\n---------')
-            if A[i][k] < l and A[k][j] < l and A[i][j] > A[i][k] + A[k][j]:
+            repeats += 1
+            print('k:', k, ' i:', i, ' j:', j)
+            if A[i][k] + A[k][j] < A[i][j]:
                 A[i][j] = A[i][k] + A[k][j]
+                path[i][j] = path[i][k]
+
+print('repeats: ', repeats)
+print('calculate repeats: ', calculate_repeats)
+
+def shortestPath(i, j):
+    if A[i][j] == l:
+        print('Path not found')
+    else:
+        s = i
+        while s != j:
+            print(nodesNames[s])
+            s = path[s][j]
+        print(nodesNames[j])
+
+shortestPath(0, 5)
+
+for row in path:
+    print(row)
 
 for i in range(0, width):
-    print(i)
     for j in range(0, width):
-        # print(A[i][j], l)
         if A[i][j] == l:
             A[i][j] = ' '
-
-nodesNames = ['A', 'B', 'C', 'D', 'E', 'F']
 
 print(nodesNames)
 for i in range(0, width):
     print(nodesNames[i], A[i])
+print('-----------\n')
+
+
+
+
